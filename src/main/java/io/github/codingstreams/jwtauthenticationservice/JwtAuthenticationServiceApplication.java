f3c4ff552daf494d51d26e2998e4b2807e88ef65
Coding Streams <akxhdev@outlook.com>
2024-01-21 12:21:52 +0530

    Initial Commit

---
+++ src/main/java/io/github/codingstreams/jwtauthenticationservice/JwtAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.codingstreams.jwtauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/codingstreams/jwtauthenticationservice/JwtAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.codingstreams.jwtauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
